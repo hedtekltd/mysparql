c3b4c0e4b331f74f8075bd8dab176e4faf5e0949
David Workman <workmad3@gmail.com>
2010-10-11 14:31:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySparql::Application.config.secret_token = '3531f4b767b790e82f7d952714c2d68b2a6063b4e8c3494a3714bd0bd4a0fed2e81f79f4f860564b79bd108c4e3e96ceb9bd53f3ee0d2455e47746ede937a497'
